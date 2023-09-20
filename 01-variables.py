37aaa336c2af59bd3516943d89ae330aef8b07e2
Paulacoria <coriapaula16@gmail.com>
2023-09-19 22:13:14 -0300

    primeras prácticas de variables

---
+++ 01-variables.py
@@ -0,0 +1,33 @@
+variable = 'valor'
+
+caja1 = 'ropa de invierno'
+caja2 = 'ropa de verano'
+caja3 = 'elementos de la cocina'
+
+caja1 = 123
+caja2 = 456
+caja3 = 789
+
+print('\nValores de tipo: int')
+print(caja1)
+print(caja2)
+print(caja3)
+
+
+variable1 = 'Esto es un contenido de tipo str'
+print(type(variable1))
+
+variable2 = 123456
+print(type(variable2))
+
+variable3 = 4.351
+print(type(variable3))
+
+variable4 = True
+print(type(variable4))
+
+variable5 = False
+print(type(variable5))
+
+
+
